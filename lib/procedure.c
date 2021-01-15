de8ea252787a49f2756ca4a5f65cc4f2cb01b914
NicolaGiardino <nicoladgg@protonmail.com>
2021-01-15 17:15:41 +0100

    V1 Published

--- lib/procedure.c
+++ lib/procedure.c
@@ -589,9 +589,9 @@ void vPrintLCD(float val[2])
 {
     lcdClear(lcdIndex);
     lcdPosition(lcdIndex, 0, 0);
-    lcdPrintf(lcdIndex, "SOC: %f%%", val[0] * 100);
+    lcdPrintf(lcdIndex, "SOC: %.2f%%", val[0] * 100);
     lcdPosition(lcdIndex, 0, 1);
-    lcdPrintf(lcdIndex, "Temp: %fC", val[1]);
+    lcdPrintf(lcdIndex, "Temp: %.2fC", val[1]);
 }
 
 #endif
